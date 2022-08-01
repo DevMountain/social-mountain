98330865948c31fc7d7a7758ee516b58df0ff3a2
lazikaz <souflakey@gmail.com>
2022-08-01 15:38:59 -0600

    finished project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from "./Post/Post"
 
 class App extends Component {
   constructor() {
@@ -19,18 +21,25 @@ class App extends Component {
   }
   
   componentDidMount() {
+    axios.get("https://practiceapi.devmountain.com/api/posts").then(results => {this.setState({posts: results.data})})
+    
+  }
+
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then(result => {this.setState({posts: result.data})})
+  }
+
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+    .then(res => {this.setState({posts: res.data})})
 
   }
 
-  updatePost() {
-  
-  }
-
-  deletePost() {
-
-  }
-
-  createPost() {
+  createPost(content) {
+    console.log(content)
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, {"text": content})
+    .then(res => {this.setState({posts: res.data})})
 
   }
 
@@ -43,8 +52,12 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost}/>
+          {
+            posts.map(post => {
+              return <Post key={post.id} text={post.text} date={post.date} updatePostFn={this.updatePost} id={post.id} deletePostFn={this.deletePost}/>
+            })
+          }
         </section>
       </div>
     );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -66,7 +66,7 @@ export default class Post extends Component {
           {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
+            <span onClick={() => this.props.deletePostFn(this.props.id)}>Delete</span>
           </div>
         </div>
 
@@ -79,7 +79,7 @@ export default class Post extends Component {
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
 
-          <span className="Post__date">- POST DATE GOES HERE</span>
+          <span className="Post__date">- {this.props.date}</span>
         </div>
 
         {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
@@ -95,10 +95,9 @@ export default class Post extends Component {
             // This has been pulled off of this.state via destructuring
             editing
             ?
-              <Edit text=""
-                    hideEdit={ this.hideEdit } />
+              <Edit text={this.props.text} hideEdit={ this.hideEdit } updatePostFn={this.props.updatePostFn} id={this.props.id}/>
             :
-              <span className="Post__text">POST TEXT GOES HERE</span>
+              <span className="Post__text">{this.props.text}</span>
           }
         </div>
 
